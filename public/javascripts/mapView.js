93e4fd5d08c0e787701d14b5d4f90a82a7a60bb0
Mitsui Yasuyuki <mitsui.yasuyuki@simplex-cn.co.jp>
2014-09-29 15:12:04 +0900

    距離を表示するように修正

--- public/javascripts/mapView.js
+++ public/javascripts/mapView.js
@@ -14,7 +14,7 @@
         initialize : function() {
             this.map = new google.maps.Map(this.$('#map').get(0), {
                 mapTypeId : google.maps.MapTypeId.ROADMAP,
-                zoom : 15,
+                zoom : 14,
                 navigationControl : true,
                 navigationControlOptions : {
                     style : google.maps.NavigationControlStyle.ANDROID
@@ -23,18 +23,17 @@
 
             this.setHeight();
             $(window).bind('resize', this.setHeight);
+            
+            this.drowJirodeLine();
 
             this.markerStore = {};
             this.memberParams = [];
             this.selfParam = {};
 
-            this.searchCurrentPosition(true);
-
-            this.drowJirodeLine();
-            this.getMembers();
+            this.update();
         },
 
-        searchCurrentPosition : function(init, callback) {
+        searchCurrentPosition : function(callback) {
             var self = this;
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
@@ -49,12 +48,10 @@
                         updateTime:new Date().getTime()
                     };
 
-                    init && self.setCenter(self.selfParam);
+                    self.setCenter(self.selfParam);
                     self.drowPositionMarker(self.selfParam);
                     self.updateJirodeElevation(self.selfParam);
-                    
-                    //$.post('/currentPosition', self.selfParam, 'json');
-                    
+
                     callback && callback();
                 }, function() {
 
@@ -64,18 +61,20 @@
         
         update : function(){
           this.deleteAllMarker();
-          this.searchCurrentPosition(true);
-          this.getMembers();
+          this.parallel([
+            this.searchCurrentPosition,
+            this.getMembers
+          ], this.drawMemberList, this);
         },
         
-        getMembers : function(){
+        getMembers : function(callback){
           var self = this;
           $.post('/getMembers', function(data){
             self.memberParams = [];
             for(var i in data){
               self.memberParams.push(data[i]);
             }
-            self.drawMemberList();
+            callback && callback();
           }, 'json')
         },
         
@@ -85,8 +84,13 @@
             var member = this.memberParams[i];
             var $member = $('<tr class="member">');
             $member.append($('<td class="name">'+member.name+'</td>'));
+            
+            var distance = this.cal_distance(this.selfParam.position, member.position);
+            $member.append($('<td class="distance"><div class="desc">現在地との距離</div><div class="val">'+this.format(distance)+'m</div></td>'));
+            
             var time = new Date(Number(member.updateTime));
             $member.append($('<td class="updateTime">('+time.getHours()+':'+time.getMinutes()+')</td>'));
+            
             $member.data('param', member);
             $('#memberList').append($member);
             this.drowPositionMarker(member);
@@ -99,15 +103,8 @@
         },
 
         drowPositionMarker : function(param) {
-            //this.deleteMarker(param);
             this.createMarker(param);
         },
-
-        // deleteMarker : function(param) {
-            // var currenctMarker = this.markerStore[param.accountId];
-            // currenctMarker && currenctMarker.setMap(null);
-            // delete this.markerStore[param.accountId];
-        // },
         
         deleteAllMarker : function() {
           for(var i in this.markerStore){
@@ -208,6 +205,84 @@
         
         getDiff: function(currentPosition, routeData){
             return Math.abs(currentPosition.latitude - routeData.lb) + Math.abs(currentPosition.longitude - routeData.mb);
+        },
+        
+        cal_distance : function (pos1, pos2){
+          var lat1 = pos1.latitude;
+          var lon1 = pos1.longitude;
+          var lat2 = pos2.latitude;
+          var lon2 = pos2.longitude;
+          
+          //ラジアンに変換
+          var a_lat = lat1 * Math.PI / 180;
+          var a_lon = lon1 * Math.PI / 180;
+          var b_lat = lat2 * Math.PI / 180;
+          var b_lon = lon2 * Math.PI / 180;
+      
+          // 緯度の平均、緯度間の差、経度間の差
+          var latave = (a_lat + b_lat) / 2;
+          var latidiff = a_lat - b_lat;
+          var longdiff = a_lon - b_lon;
+      
+          //子午線曲率半径
+          //半径を6335439m、離心率を0.006694で設定してます
+          var meridian = 6335439 / Math.sqrt(Math.pow(1 - 0.006694 * Math.sin(latave) * Math.sin(latave), 3));    
+      
+          //卯酉線曲率半径
+          //半径を6378137m、離心率を0.006694で設定してます
+          var primevertical = 6378137 / Math.sqrt(1 - 0.006694 * Math.sin(latave) * Math.sin(latave));     
+      
+          //Hubenyの簡易式
+          var x = meridian * latidiff;
+          var y = primevertical * Math.cos(latave) * longdiff;
+      
+          return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+        },
+        
+        parallel:function(tasks, callback, ctx){
+          var counter = tasks.length;
+          for(var i = 0; i < tasks.length; i++){
+            tasks[i].call(ctx, function(){
+              counter--;
+              if(counter === 0){
+                callback && callback.call(ctx);
+              }
+            });
+          }
+        },
+        
+        /**
+         * 左文字埋め
+         * 例）
+         * format(1234.56) => 1,234
+         * format(1234.567, 2, ',', ' ') => 1 234,57
+         * 
+         * @param {String or Number} number 対象数字
+         * @param {Number} decimals 有効な小数点（デフォルト：0）
+         * @param {String} dec_point 小数点の区切り（デフォルト：.）
+         * @param {String} thousands_sep 数字の区切り（デフォルト：,）
+         */
+        format:function number_format (number, decimals, dec_point, thousands_sep) {
+          number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+          var n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+            s = '',
+            toFixedFix = function (n, prec) {
+              var k = Math.pow(10, prec);
+              return '' + Math.round(n * k) / k;
+            };
+          // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+          s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+          if (s[0].length > 3) {
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+          }
+          if ((s[1] || '').length < prec) {
+            s[1] = s[1] || '';
+            s[1] += new Array(prec - s[1].length + 1).join('0');
+          }
+          return s.join(dec);
         }
     });
 })();
